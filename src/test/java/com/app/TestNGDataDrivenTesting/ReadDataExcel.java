c8ce141e241bf0a3db33c38e2652333deb19d1f1
Bollu <Bollu@gmail.com>
2023-10-10 09:24:12 +0000

    done

---
+++ src/test/java/com/app/TestNGDataDrivenTesting/ReadDataExcel.java
@@ -0,0 +1,5 @@
+package com.app.TestNGDataDrivenTesting;
+
+public class ReadDataExcel {
+
+}
---
+++ src/test/java/com/app/TestNGDataDrivenTesting/XLS_DataProvider.java
@@ -0,0 +1,15 @@
+package com.app.TestNGDataDrivenTesting;
+
+public class XLS_DataProvider {
+
+	public static Object[][] getTestData1(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object[][] getTestData(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
